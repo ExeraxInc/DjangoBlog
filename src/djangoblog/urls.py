3512f09e8b2a6f87dac22c3297e0327aa581fac2
Valentin Valev <valentins613@gmail.com>
2018-01-20 16:47:27 +0000

    Few changes made prior to futher improvements
    *Posts urls are replaced by a specific path to retrieve the data and generate a special link according to the CRUD method.
    *Few more definitions about CRUD has been made.

--- src/djangoblog/urls.py
+++ src/djangoblog/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from posts import views
+from django.urls import include, path
+from posts import views, urls
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path(r'^posts/', views.posts_home),
+    path('admin/', admin.site.urls),
+    path('posts/', include(urls)),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -2,5 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def posts_home(request):
-	return HttpResponse("<h1>Hello</h1>")
+def posts_create(request): #create a post
+	return HttpResponse("<h1>Create</h1>")
+
+def posts_detail(request): #retrieve a data
+	return HttpResponse("<h1>Detail</h1>")
+
+def posts_list(request): #list an item
+	return HttpResponse("<h1>List</h1>")
+
+def posts_update(request): #update the post
+	return HttpResponse("<h1>Update</h1>")
+
+def posts_delete(request): #delete the post
+	return HttpResponse("<h1>Delete</h1>")
+
